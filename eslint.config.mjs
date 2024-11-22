82b640b16862da9382f912b82eb539a53edfb760
Lasse Fister <commander@graphicore.de>
2024-11-22 02:12:06 +0100

    [eslint] more rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,19 @@
+import globals from "globals";
+
 export default [{
   "languageOptions": {
       "ecmaVersion": "latest",
-      "sourceType": "module"
+      "sourceType": "module",
+      "globals": {
+        ...globals.browser
+      }
   },
   "rules": {
       // sparse arrays can be a subtle error, but often
       // I want to use them.
-      "no-sparse-arrays": "off"
+      "no-sparse-arrays": "off",
+      "no-undef": ["error", { "typeof": true }],
+      "no-unused-vars": "warn"
   },
 }];
 
