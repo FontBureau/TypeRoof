3e0c205df335d189a2bb2262dd2f359c95b44f55
Lasse Fister <commander@graphicore.de>
2025-09-18 14:50:43 +0200

    [stylelint] disable rule "no-duplicate-selectors" as it breaks clustering rules by topic.

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,6 +2,7 @@ export default {
   "extends": ["stylelint-config-standard"],
   "rules": {
     "no-descending-specificity": null,
-    "selector-class-pattern": null
+    "selector-class-pattern": null,
+    "no-duplicate-selectors": null, // it breaks clustering rules by topic
   }
 };
