50a6fbf988958013c564128f3f857a4846b47f3e
graphicore <graphicore@users.noreply.github.com>
2025-10-30 08:33:10 +0000

    deploy: 7946872af9bfa8f624287542961c8feddfefae49

--- lib/js/components/react-examples/react-counter.jsx
+++ lib/js/components/react-examples/react-counter.jsx
@@ -3,74 +3,74 @@ import PropTypes from "prop-types";
 import { useMetamodel } from "../react-integration.jsx";
 
 function CounterDisplay({ counterPath }) {
-  const [{ count }] = useMetamodel([[counterPath, "count"]]);
+    const [{ count }] = useMetamodel([[counterPath, "count"]]);
 
-  return (
-    <div>
-      <h2>Counter Display</h2>
+    return (
+        <div>
+            <h2>Counter Display</h2>
 
-      <p>{count.value}</p>
-    </div>
-  );
+            <p>{count.value}</p>
+        </div>
+    );
 }
 CounterDisplay.propTypes = {
-  counterPath: PropTypes.string.isRequired,
+    counterPath: PropTypes.string.isRequired,
 };
 
 function CounterControls({ counterPath }) {
-  const [, widgetBus] = useMetamodel();
-  const increment = () => {
-    widgetBus.changeState(() => {
-      const counter = widgetBus.getEntry(counterPath);
-      counter.value = counter.value + 1;
-    });
-  };
+    const [, widgetBus] = useMetamodel();
+    const increment = () => {
+        widgetBus.changeState(() => {
+            const counter = widgetBus.getEntry(counterPath);
+            counter.value = counter.value + 1;
+        });
+    };
 
-  const decrement = () => {
-    widgetBus.changeState(() => {
-      const counter = widgetBus.getEntry(counterPath);
-      counter.value = counter.value - 1;
-    });
-  };
+    const decrement = () => {
+        widgetBus.changeState(() => {
+            const counter = widgetBus.getEntry(counterPath);
+            counter.value = counter.value - 1;
+        });
+    };
 
-  const reset = () => {
-    widgetBus.changeState(() => {
-      widgetBus.getEntry(counterPath).value = 0;
-    });
-  };
+    const reset = () => {
+        widgetBus.changeState(() => {
+            widgetBus.getEntry(counterPath).value = 0;
+        });
+    };
 
-  return (
-    <div className="flex justify-center space-x-4">
-      <button onClick={increment} type="button">
-        Increment
-      </button>
+    return (
+        <div className="flex justify-center space-x-4">
+            <button onClick={increment} type="button">
+                Increment
+            </button>
 
-      <button onClick={decrement} type="button">
-        Decrement
-      </button>
+            <button onClick={decrement} type="button">
+                Decrement
+            </button>
 
-      <button onClick={reset} type="button">
-        Reset
-      </button>
-    </div>
-  );
+            <button onClick={reset} type="button">
+                Reset
+            </button>
+        </div>
+    );
 }
 CounterControls.propTypes = {
-  counterPath: PropTypes.string.isRequired,
+    counterPath: PropTypes.string.isRequired,
 };
 
 // The main application component.
 function Counter({ counterPath }) {
-  return (
-    <div>
-      <CounterDisplay counterPath={counterPath} />
+    return (
+        <div>
+            <CounterDisplay counterPath={counterPath} />
 
-      <CounterControls counterPath={counterPath} />
-    </div>
-  );
+            <CounterControls counterPath={counterPath} />
+        </div>
+    );
 }
 Counter.propTypes = {
-  counterPath: PropTypes.string.isRequired,
+    counterPath: PropTypes.string.isRequired,
 };
 
 export { Counter };
