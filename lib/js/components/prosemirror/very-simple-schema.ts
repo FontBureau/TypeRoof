9bb7b92395a4c93e85c4ba7531533ea327f3cec7
Lasse Fister <commander@graphicore.de>
2025-12-02 15:59:03 +0100

    [ts] run prettier --write on all existing .ts files

--- lib/js/components/prosemirror/very-simple-schema.ts
+++ lib/js/components/prosemirror/very-simple-schema.ts
@@ -1,7 +1,7 @@
-import {SchemaSpec, NodeSpec, DOMOutputSpec} from "prosemirror-model"
-import {schemaSpec as defaultSchemaSpec} from "./default-schema"
+import { SchemaSpec, NodeSpec, DOMOutputSpec } from "prosemirror-model";
+import { schemaSpec as defaultSchemaSpec } from "./default-schema";
 
-const pDOM: DOMOutputSpec = ["p",  0];
+const pDOM: DOMOutputSpec = ["p", 0];
 
 export const nodes = {
     doc: defaultSchemaSpec.nodes.doc,
@@ -10,15 +10,15 @@ export const nodes = {
     /// A plain paragraph textblock. Represented in the DOM
     /// as a `<p>` element.
     paragraph: {
-      content: "inline*",
-      group: "block",
-      parseDOM: [{tag: "p"}],
-      toDOM() { return pDOM }
-    } as NodeSpec
-}
+        content: "inline*",
+        group: "block",
+        parseDOM: [{ tag: "p" }],
+        toDOM() {
+            return pDOM;
+        },
+    } as NodeSpec,
+};
 
 export const marks = {};
 
-export const schemaSpec = {nodes, marks} as SchemaSpec;
-
-
+export const schemaSpec = { nodes, marks } as SchemaSpec;
