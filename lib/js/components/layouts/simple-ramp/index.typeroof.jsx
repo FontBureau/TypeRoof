403cda47ff81db6240fe79977fd5582e8022336e
Eduardo Omine <eduardo.omine@gmail.com>
2026-04-30 15:14:28 -0300

    Add simple ramp layout

---
+++ lib/js/components/layouts/simple-ramp/index.typeroof.jsx
@@ -0,0 +1,265 @@
+import {
+    _BaseContainerComponent,
+    SimpleProtocolHandler,
+} from "../../basics.mjs";
+import { _BaseLayoutModel } from "../../main-model.mjs";
+import {
+    PathModelOrEmpty,
+    Path,
+    BooleanModel,
+    CoherenceFunction,
+    deserializeSync,
+    SERIALIZE_OPTIONS,
+    SERIALIZE_FORMAT_OBJECT,
+} from "../../../metamodel.mjs";
+import {
+    TypeSpecModel,
+    StylePatchesMapModel,
+} from "../../type-spec-models.mjs";
+import {
+    ProseMirrorSchemaModel,
+    NodeSpecToTypeSpecMapModel,
+    NodeModel,
+} from "../../prosemirror/models.typeroof.jsx";
+import { Collapsible, UICheckboxInput } from "../../generic.mjs";
+import { GENERIC } from "../../registered-properties-definitions.mjs";
+import {
+    isInheritingPropertyFn,
+    getRegisteredPropertySetup,
+} from "../../registered-properties.mjs";
+import {
+    _getTypeSpecDefaultsMap,
+    TYPE_SPEC_PROPERTIES_GENERATORS,
+} from "../ramp/get-type-spec-defaults-map.mjs";
+import { StylePatchSourcesMeta } from "../ramp/style-patch-sources-meta.typeroof.jsx";
+import { TypeSpecMeta } from "../ramp/type-spec-meta.typeroof.jsx";
+import { TypeSpecPropertiesManager } from "../ramp/type-spec-properties-manager.typeroof.jsx";
+import { UIStylePatchesMap } from "../ramp/ui-style-patches-map.typeroof.jsx";
+import { StylePatchPropertiesManager } from "../ramp/style-patch-properties-manager.typeroof.jsx";
+import { ProseMirrorContext } from "../ramp/prose-mirror-context.typeroof.jsx";
+import DEFAULT_STATE from "../../../../assets/typespec-ramp-initial-state.json" with { type: "json" };
+
+//  We can't create the self-reference directly
+//, TypeSpecModelMap: TypeSpec.get('children') === _AbstractOrderedMapModel.createClass('TypeSpecModelMap', TypeSpec)
+const SimpleRampModel = _BaseLayoutModel.createClass(
+    "SimpleRampModel",
+    // The root TypeSpec
+    ["typeSpec", TypeSpecModel],
+    ["editingTypeSpec", PathModelOrEmpty],
+    // could potentially be a struct with some coherence logic etc.
+    // for the actual data
+    ["stylePatchesSource", StylePatchesMapModel],
+    ["editingStylePatch", PathModelOrEmpty],
+    ["proseMirrorSchema", ProseMirrorSchemaModel],
+    ["editingNodeSpecPath", PathModelOrEmpty],
+    ["nodeSpecToTypeSpec", NodeSpecToTypeSpecMapModel],
+    // the root of all typeSpecs
+    ["document", NodeModel],
+    ["showParameters", BooleanModel],
+    CoherenceFunction.create(
+        [
+            "document",
+            "typeSpec",
+            "stylePatchesSource",
+            "proseMirrorSchema",
+            "nodeSpecToTypeSpec",
+        ],
+        function initTypeSpec({
+            typeSpec,
+            document,
+            stylePatchesSource,
+            proseMirrorSchema,
+            nodeSpecToTypeSpec,
+        }) {
+            // if typeSpec and document are empty
+            if (
+                document.get("content").size === 0 &&
+                typeSpec.get("children").size === 0 &&
+                stylePatchesSource.size === 0
+            ) {
+                for (const [Model, target, data] of [
+                    [NodeModel, document, DEFAULT_STATE.document],
+                    [TypeSpecModel, typeSpec, DEFAULT_STATE.typeSpec],
+                    [
+                        StylePatchesMapModel,
+                        stylePatchesSource,
+                        DEFAULT_STATE.stylePatchesSource,
+                    ],
+                    [
+                        ProseMirrorSchemaModel,
+                        proseMirrorSchema,
+                        DEFAULT_STATE.proseMirrorSchema,
+                    ],
+                    [
+                        NodeSpecToTypeSpecMapModel,
+                        nodeSpecToTypeSpec,
+                        DEFAULT_STATE.nodeSpecToTypeSpec,
+                    ],
+                ]) {
+                    const serializeOptions = Object.assign(
+                            {},
+                            SERIALIZE_OPTIONS,
+                            {
+                                format: SERIALIZE_FORMAT_OBJECT,
+                            },
+                        ),
+                        newItem = deserializeSync(
+                            Model,
+                            target.dependencies,
+                            data,
+                            serializeOptions,
+                        );
+                    for (const [key, enrty] of newItem.entries())
+                        target.set(key, enrty);
+                }
+            }
+        },
+    ),
+);
+
+class SimpleRampController extends _BaseContainerComponent {
+    constructor(widgetBus, _zones) {
+        // BUT: we may need a mechanism to handle typeSpec inheritance!
+        // widgetBus.wrapper.setProtocolHandlerImplementation(
+        //    ...SimpleProtocolHandler.create('animationProperties@'));
+        const propertiesManagerContainer = widgetBus.domTool.createElement(
+                "div",
+                {
+                    class: "properties-manager",
+                },
+            ),
+            // To have this first within editorManagerContainer.
+            proseMirrorEditorMenuContainer = widgetBus.domTool.createElement(
+                "div",
+                { class: "editor-manager-prosemirror" },
+            ),
+            editorManagerContainer = widgetBus.domTool.createElement(
+                "div",
+                {
+                    class: "editor-manager",
+                },
+                proseMirrorEditorMenuContainer,
+            ),
+            zones = new Map([
+                ..._zones,
+                ["properties-manager", propertiesManagerContainer],
+                ["editor-manager", editorManagerContainer],
+                ["prose-mirror-editor-menu", proseMirrorEditorMenuContainer],
+            ]),
+            typeSpecRelativePath = Path.fromParts(".", "typeSpec"),
+            originTypeSpecPath = widgetBus.rootPath.append(
+                ...typeSpecRelativePath,
+            );
+        widgetBus.wrapper.setProtocolHandlerImplementation(
+            ...SimpleProtocolHandler.create("typeSpecProperties@"),
+        );
+
+        // the source style patches
+        widgetBus.wrapper.setProtocolHandlerImplementation(
+            // does not raise when not found, instead returns null
+            ...SimpleProtocolHandler.create("stylePatchProperties@", {
+                notFoundFallbackValue: null,
+            }),
+        );
+
+        // the linked stylePatchProperties@ plus typeSpecProperties@
+        widgetBus.wrapper.setProtocolHandlerImplementation(
+            ...SimpleProtocolHandler.create("styleLinkProperties@"),
+        );
+        // widgetBus.insertElement(stageManagerContainer);
+        super(widgetBus, zones);
+
+        const typeSpecDefaultsMap = _getTypeSpecDefaultsMap(
+            widgetBus.getEntry(originTypeSpecPath).dependencies,
+        );
+
+        const widgets = [
+            [
+                {
+                    rootPath: widgetBus.rootPath,
+                },
+                [["stylePatchesSource", "collection"]],
+                StylePatchSourcesMeta,
+                zones,
+            ],
+            [
+                {
+                    rootPath: typeSpecRelativePath,
+                },
+                [
+                    [".", "typeSpec"],
+                    [
+                        widgetBus.rootPath
+                            .append("stylePatchesSource")
+                            .toString(),
+                        "stylePatchesSource",
+                    ],
+                    // special, reqired only for the root instance
+                    ["/font", "rootFont"],
+                ],
+                TypeSpecMeta,
+                zones,
+                TYPE_SPEC_PROPERTIES_GENERATORS,
+                isInheritingPropertyFn,
+                typeSpecDefaultsMap,
+            ],
+            [
+                { zone: "main" },
+                [],
+                Collapsible,
+                "Editor",
+                editorManagerContainer,
+            ],
+            [
+                { zone: "main" },
+                [],
+                Collapsible,
+                "TypeSpec Properties",
+                propertiesManagerContainer,
+            ],
+            [
+                {},
+                [
+                    ["editingTypeSpec", "typeSpecPath"],
+                    ["typeSpec/children", "children"],
+                    ["typeSpec", "rootTypeSpec"],
+                ],
+                TypeSpecPropertiesManager,
+                new Map([...zones, ["main", propertiesManagerContainer]]),
+            ],
+            [
+                {},
+                [],
+                ProseMirrorContext,
+                zones,
+                // proseMirrorSettings
+                { zone: "layout" },
+                originTypeSpecPath,
+                // menuSettings
+                { zone: "prose-mirror-editor-menu" },
+            ],
+            [
+                { zone: "editor-manager" },
+                [["showParameters", "value"]],
+                UICheckboxInput,
+                "show-parameters", // classToken
+                getRegisteredPropertySetup(`${GENERIC}showParameters`).label, //label
+            ],
+        ];
+        this._initWidgets(widgets);
+    }
+    update(...args) {
+        this.widgetBus.wrapper
+            .getProtocolHandlerImplementation("typeSpecProperties@")
+            .resetUpdatedLog();
+        super.update(...args);
+    }
+    initialUpdate(...args) {
+        this.widgetBus.wrapper
+            .getProtocolHandlerImplementation("typeSpecProperties@")
+            .resetUpdatedLog();
+        super.initialUpdate(...args);
+    }
+}
+
+export { SimpleRampModel as Model, SimpleRampController as Controller };
