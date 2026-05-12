a6bb72f92a1eca330db79fd9705624e8eac051e9
graphicore <graphicore@users.noreply.github.com>
2026-05-12 15:16:40 +0000

    deploy: 2c5a4be5771048894b3d287d6eff0a3455f72826

--- lib/js/components/layouts/ramp/shared.typeroof.jsx
+++ lib/js/components/layouts/ramp/shared.typeroof.jsx
@@ -1,9 +1,42 @@
+import { DynamicTag } from "../../generic.mjs";
 import {
     _CommonContainerComponent,
     _BaseDynamicCollectionContainerComponent,
 } from "../../basics.mjs";
 import { Path, getEntry } from "../../../metamodel.mjs";
 import { StaticTag } from "../../generic.mjs";
+
+export class MapSelectButton extends DynamicTag {
+    constructor(widgetBus, tag, attr, eventListeners, ...restArgs) {
+        super(widgetBus, tag, attr, ...restArgs);
+        for (const eventListener of eventListeners)
+            this.element.addEventListener(...eventListener);
+    }
+
+    _setActive(pathEntry) {
+        let shouldBeActive = false;
+        if (!pathEntry.isEmpty) {
+            const myKey = this.widgetBus.rootPath.parts.at(-1), // .../{key}
+                path = pathEntry.value,
+                selectedKey = path.parts.at(-1); // ./key
+            shouldBeActive = myKey === selectedKey;
+        }
+        this.element.classList[shouldBeActive ? "add" : "remove"]("active");
+    }
+
+    update(changedMap) {
+        super.update(changedMap);
+        if (changedMap.has("activePath")) {
+            const pathEntry = changedMap.get("activePath");
+            this._setActive(pathEntry);
+        }
+
+        if (changedMap.has("data"))
+            this.element.textContent = this._formatter(
+                changedMap.get("data").value,
+            );
+    }
+}
 
 /**
  * FIXME: this is also kind of a repeated pattern TypeSpecPropertiesManager
