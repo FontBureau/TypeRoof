f180aeff5221ca9a2ade7eac77f622471d40beea
Lasse Fister <commander@graphicore.de>
2026-06-08 16:23:50 +0200

    [layouts/videoproof] bring back showCellBoxes and cellAlignment and enable in videoproof-contextual.

--- lib/js/components/actors/videoproof-contextual/index.typeroof.jsx
+++ lib/js/components/actors/videoproof-contextual/index.typeroof.jsx
@@ -519,7 +519,10 @@ export class VideoproofContextualActorRenderer extends _BaseComponent {
 
     update(changedMap) {
         const propertiesData = [
+            // [fullKey, cssProperty, unit, cleanFn]
             ["numericProperties/z-index", "z-index", "", Math.round],
+            ['generic/showCellBoxes', '--cell-boxes-outline', '', (val)=>val === true ? '1px solid' : ''],
+            ['generic/cellAlignment', '--cell-alignment', '', ],
         ];
 
         const font = (
