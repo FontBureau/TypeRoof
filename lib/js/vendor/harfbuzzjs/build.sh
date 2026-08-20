8b0157b813f60c234d335c430bf1ecc59981deb8
graphicore <graphicore@users.noreply.github.com>
2026-08-20 15:02:56 +0000

    deploy: 85fe8d3c98b20921e58a2169d52933397510e458

--- lib/js/vendor/harfbuzzjs/build.sh
+++ lib/js/vendor/harfbuzzjs/build.sh
@@ -1,32 +1,34 @@
 #! /usr/bin/env bash
 
+# Build and vendor harfbuzzjs v1.x from upstream.
+#
+# As of v1.x all former custom patches of the graphicore fork are upstream
+# (stack runtime methods, hb_font_get_{glyph,h}_extents exports, string
+# alignment fix) and the build configuration (config-override.h) is part
+# of the upstream repo, so we track origin/main directly.
+#
+# Requires: emscripten (em++) and node/npm.
+# Run from this directory. Copies the resulting artifacts in place:
+#   index.mjs, index.d.mts, harfbuzz.js, harfbuzz.wasm
+
+set -e
+
 mkdir .build -p
 cd .build
 
 if [ -d harfbuzzjs ]; then
    cd harfbuzzjs
-   git pull --recurse-submodules --rebase
+   git fetch origin
+   git checkout main
+   git pull --ff-only --recurse-submodules origin main
 else
-   # use js-callbacks as branch as it has just the features argument
-   # of shape implemented. when the PR is through, this can fetch master
-   # again ...
-   # https://github.com/harfbuzz/harfbuzzjs/pull/97
-   # Added custom function exports, that the official version doesn't
-   # export (yet?)
-   git clone -b js-callbacks --single-branch --recursive --depth 1 -b TypeRoof-main git@github.com:graphicore/harfbuzzjs.git
+   git clone --single-branch --recursive --depth 1 https://github.com/harfbuzz/harfbuzzjs.git
    cd harfbuzzjs
 fi
 
-./build.sh
+make
 cd ../..
-cp .build/harfbuzzjs/hb.wasm hb.wasm
-echo 'define(function(require, exports, module){' > hbjs.js
-cat .build/harfbuzzjs/hbjs.js >> hbjs.js
-echo '});' >> hbjs.js
-
-
-cat .build/harfbuzzjs/hbjs.js > hbjs.mjs
-echo 'export default hbjs;' >> hbjs.mjs
-
-cat .build/harfbuzzjs/hb.js > hb.mjs
-echo 'export default Module;' >> hb.mjs
+cp .build/harfbuzzjs/dist/index.mjs index.mjs
+cp .build/harfbuzzjs/dist/index.d.mts index.d.mts
+cp .build/harfbuzzjs/dist/harfbuzz.js harfbuzz.js
+cp .build/harfbuzzjs/dist/harfbuzz.wasm harfbuzz.wasm
