3930af46101e18c2d74ea758308ccbf458188584
Eduardo Omine <eduardo.omine@gmail.com>
2026-09-08 19:43:17 -0300

    Mock ResizeObserver on tests

---
+++ lib/js/tests/setup.ts
@@ -0,0 +1,11 @@
+import { vi } from "vitest";
+
+const ResizeObserverMock = vi.fn(
+    class {
+        observe = vi.fn();
+        unobserve = vi.fn();
+        disconnect = vi.fn();
+    },
+);
+
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
